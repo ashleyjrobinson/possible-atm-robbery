c69f23e369f8ecfebe93b10ef113fe47ac8ef241
ashleyjrobinson <ashjrobinson95@gmail.com>
2024-02-08 00:39:51 +0000

    feat: :sparkles: Client rewrite

--- client/main.lua
+++ client/main.lua
@@ -1,83 +1,13 @@
+lib.locale()
 local config = require('shared.config')
+local policeCount = nil 
 
-local QBCore = exports['qb-core']:GetCoreObject()
-local copsCalled = false
-local CurrentCops = 0
-local PlayerJob = {}
-local onDuty = false
-
-RegisterNetEvent('QBCore:Client:OnPlayerLoaded', function()
-    PlayerJob = QBCore.Functions.GetPlayerData().job
-    onDuty = true
-end)
-
-RegisterNetEvent('QBCore:Client:SetDuty', function(duty)
-    onDuty = duty
-end)
-
-RegisterNetEvent('QBCore:Client:OnJobUpdate', function(JobInfo)
-    PlayerJob = JobInfo
-    onDuty = true
-end)
-
-RegisterNetEvent('police:SetCopCount', function(amount)
-    CurrentCops = amount
-end)
-
-local function PoliceCall()
+local function policeCall()
+    if config.Dispatch == "ps-dispatch" then
      exports['ps-dispatch']:ATMHacking()
-end
-
-function LoadAnim(dict)
-    while not HasAnimDictLoaded(dict) do
-      RequestAnimDict(dict)
-      Wait(10)
+    else
+    -- Add your custom export here
     end
-  end
-
-  
-function LoadPropDict(model)
-    while not HasModelLoaded(GetHashKey(model)) do
-        RequestModel(GetHashKey(model))
-        Wait(10)
-    end
-end
-    
-local PlayerHasProp = false
-local PlayerProps = {}
-
-function AddPropToPlayer(prop1, bone, off1, off2, off3, rot1, rot2, rot3)
-    local Player = PlayerPedId()
-    local x, y, z = table.unpack(GetEntityCoords(Player))
-
-    if not HasModelLoaded(prop1) then
-        LoadPropDict(prop1)
-    end
-
-    prop = CreateObject(GetHashKey(prop1), x, y, z + 0.2, true, true, true)
-    AttachEntityToEntity(prop, Player, GetPedBoneIndex(Player, bone), off1, off2, off3, rot1, rot2, rot3, true, true, false, true, 1, true)
-    table.insert(PlayerProps, prop)
-    PlayerHasProp = true
-    SetModelAsNoLongerNeeded(prop1)
-end
-
-function AnimMode()
-    Animation = true
-    local Player = PlayerPedId()
-    local AnimDict = "amb@code_human_wander_texting_fat@male@base"
-    local Anim = "static"
-    LoadAnim(AnimDict)
-    local Prop = 'ch_prop_ch_phone_ing_01a'
-    local PropBone = 28422
-    AddPropToPlayer(Prop, PropBone, 0.0, -0.02, 0.0, 0.0, 0.0, 0.0)
-    TaskPlayAnim(GetPlayerPed(-1), AnimDict, Anim, 2.0, 8.0, -1, 53, 0, false, false, false)
-end
-
-function DestroyAllProps()
-    for _, v in pairs(PlayerProps) do
-        DeleteEntity(v)
-    end
-    PlayerHasProp = false
 end
 
 local modelsATM = {
@@ -89,32 +19,31 @@ local modelsATM = {
 
 local optionATM = {
     {
-        name = 'possible-atm:hackATM',
-        event = 'possible-atm:startHack',
-        icon = 'fa-solid fa-user-ninja',
-        label = 'Hack ATM',
+        name = 'HackATM',
+        icon = config.LabelIcon,
+        label = locale('target_label'),
         items = config.RequiredItem,
         distance = 2.5,
         onSelect = function()
-            TriggerEvent('possible-atm-robbery:startHack')
+            TriggerEvent('possible-atm-robbery:client:startHack')
         end
     }
 }
 
-local function InitializeTargetSystem()
-    exports.ox_target:addModel(modelsATM, optionATM)
-end
+exports.ox_target:addModel(modelsATM, optionATM)
 
-InitializeTargetSystem()
-
-local function OnHackDone(success)
+local function onHackDone(success)
     ClearPedTasks(PlayerPedId())
     if success then
-        DestroyAllProps()
+        if config.Emotes == 'rpemotes' then
+            exports["rpemotes"]:EmoteCancel(forceCancel)
+        else
+            exports.scully_emotemenu:cancelEmote()
+        end
         TriggerEvent('mhacking:hide')
         if lib.progressBar({
-            duration = 7500,
-            label = 'Looting ATM Machine',
+            duration = config.ProgressDuration,
+            label = locale('looting_progress_label'),
             useWhileDead = false,
             canCancel = true,
             disable = {
@@ -131,53 +60,71 @@ local function OnHackDone(success)
             },
         }) then
             lib.notify({
-                title = 'Success',
-                description = 'You successfully hacked the ATM and grabbed the loot!',
+                title = locale('looting_success_title'),
+                description = locale('looting_success_description'),
                 type = 'success',
-                position = 'top',
+                position = config.NotifPosition,
+                icon = config.NotifIcon
             })
-            TriggerEvent('possible-atm-robbery:success')
+            TriggerEvent('possible-atm-robbery:client:success')
         else
             lib.cancelProgress()
             lib.notify({
-                title = 'Signal Lost',
-                description = 'ATM Signal lost? Try again!',
-                type = 'error'
+                title = locale('looting_signal_lost_title'),
+                description = locale('looting_signal_lost_description'),
+                type = 'error',
+                position = config.NotifPosition,
+                icon = config.NotifIcon
             })
         end
     else
         lib.notify({
-            title = 'Failed',
-            description = 'Failed to hack ATM, signal lost!',
-            type = 'error'
+            title = locale('looting_failed_title'),
+            description = locale('looting_failed_description'),
+            type = 'error',
+            position = config.NotifPosition,
+            icon = config.NotifIcon
         })
         TriggerEvent("mhacking:hide")
-        DestroyAllProps()
+        if config.Emotes == 'rpemotes' then
+            exports["rpemotes"]:EmoteCancel(forceCancel)
+        else
+            exports.scully_emotemenu:cancelEmote()
+        end
     end
 end
 
-
-RegisterNetEvent('possible-atm-robbery:startHack')
-AddEventHandler('possible-atm-robbery:startHack', function()
-    QBCore.Functions.TriggerCallback('possible-atm-robbery:server:getCops', function(cops)
-        if cops >= config.MinimumPolice then
-            AnimMode()
-            PoliceCall()
-            TriggerEvent("mhacking:show")
-            TriggerEvent("mhacking:start", math.random(6, 7), math.random(12, 15), OnHackDone)
-        else
-            lib.notify({
-                title = 'Not right now.',
-                description = 'Not enough police online!',
-                type = 'error'
-            })
-        end
-    end)
+RegisterNetEvent('possible-atm-robbery:client:receivePoliceCount', function(count)
+    policeCount = count
+    if config.Debug then
+        print('Police count: ' .. policeCount)
+    end
 end)
 
-RegisterNetEvent('possible-atm-robbery:success')
-AddEventHandler('possible-atm-robbery:success', function()
-    TriggerServerEvent('possible-atm-robbery:removeRequiredItem')
-    TriggerServerEvent('possible-atm-robbery:giveReward')
+RegisterNetEvent('possible-atm-robbery:client:startHack', function()
+    TriggerServerEvent('possible-atm-robbery:server:requestPoliceCount')
+    Wait(500)
+    if policeCount >= config.MinimumPolice then
+            if config.Emotes == 'rpemotes' then
+                exports["rpemotes"]:EmoteCommandStart("phone")
+            else
+                exports.scully_emotemenu:playEmoteByCommand('phone')
+            end
+            policeCall()
+            TriggerEvent("mhacking:show")
+            TriggerEvent("mhacking:start", math.random(6, 7), math.random(12, 15), onHackDone)
+        else
+            lib.notify({
+                title = locale('not_enough_police_title'),
+                description = locale('not_enough_police_description'),
+                type = 'error',
+                position = config.NotifPosition,
+                icon = config.NotifIcon
+            })
+     end
+end)
+
+RegisterNetEvent('possible-atm-robbery:client:success', function()
+    TriggerServerEvent('possible-atm-robbery:server:giveReward', source)
 end)
 
